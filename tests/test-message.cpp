567905562b355b5ac5ed70e60cd36eb8a5b92187
Deamhan <workmailpk@gmail.com>
2020-10-27 23:32:08 +0300

    minimal messages test has been added

---
+++ tests/test-message.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+
+#include "ipc.hpp"
+
+int main()
+{
+    std::string s1("Test"), s2;
+    int32_t i1 = 123, i2 = 0;
+    char c1 = 'T', c2 = '\0';
+    
+    ipc::out_message out;
+    out << s1 << c1 << i1;
+    
+    ipc::in_message in;
+    const auto& out_data = out.get_data();
+    std::copy(out_data.begin(), out_data.end(), in.get_data().begin());
+    
+    in >> s2 >> c2 >> i2;
+    
+    return (s1 == s2 && c1 == c2 && i1 == i2) ? 0 : 1;
+}
